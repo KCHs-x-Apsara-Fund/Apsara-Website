c5c8abe370fef756133120a800a28f08c6ecf350
Mekmun Sopheaktra <Mekmunsopheaktra@gmail.com>
2023-02-06 21:25:39 +0700

    init

---
+++ components/Footer.vue
@@ -0,0 +1,7 @@
+<template>
+  <footer class="footer bg-light py-3 shadow mt-auto">
+    <div class="container d-flex justify-content-between">
+      <span>Nuxt.js + Bootstrap 5</span>
+    </div>
+  </footer>
+</template>
