b297bb734974f3fe47909ac13d7fb54a836f768a
apsara-fund <125146515+apsara-fund@users.noreply.github.com>
2023-02-12 15:37:34 +0700

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,7 +15,7 @@ export default {
 		meta: [
 			{ charset: 'utf-8' },
 			{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
-			{ hid: 'description', name: 'description', content: 'Apsara Fund Management' }
+			{ hid: 'description', name: 'description', content: 'Apsara Fund Management' },
       { hid: 'og:title', name: 'og:title', content: 'Apsara Fund' },
       { hid: 'og:image', name: 'og:image', content: 'https://apsarafund.netlify.app/assets/img/og.png' },
 		],
