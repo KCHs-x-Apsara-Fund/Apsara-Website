da96b624dd086c36cc8c9eea3b49d55d446f0b8e
Chandarith <darith0001@gmail.com>
2023-02-27 21:13:01 +0700

    update event interface

--- pages/event_news.vue
+++ pages/event_news.vue
@@ -28,102 +28,68 @@
                 <div class="row">
                   <div class="col-xl-12 col-lg-12 col-12 our-blog">
                     <div class="post-wrapper row px-4">
-                      <div class="col-md-4 col-12 mb-4">
+                      <div
+                        v-for="event in events"
+                        :key="event.title"
+                        class="col-md-12 col-12 mb-4"
+                      >
                         <div class="single-blog">
-                          <div class="image-box">
-                            <img src="images/blog/3.jpg" alt="" />
-                            <div class="overlay">
-                              <a href="#" class="date">Feb 06, 2018</a>
+                          <div class="row">
+                            <div class="col-12 col-md-3">
+                              <div class="image-box">
+                                <img src="images/event/1.jpg" alt="" />
+                              </div>
+                              <!-- /.image-box -->
+                            </div>
+                            <div class="col-12 col-md-9">
+                              <div class="post-meta">
+                                <h5 class="title">
+                                  <a
+                                    v-if="event.title.length < 70"
+                                    href="event-details.html"
+                                    >{{ event.title }}</a
+                                  >
+                                  <a v-else href="event-details.html">{{
+                                    event.title.substring(0, 70) + "...."
+                                  }}</a>
+                                </h5>
+                                <div>
+                                  <div class="row">
+                                    <div class="col-12 col-md-3 px-auto">
+                                      <a href="#" class="date">
+                                        <i class="fa fa-calendar"></i>
+                                        {{ event.date }}</a
+                                      >
+                                    </div>
+                                    <div class="col-12 col-md-2">
+                                      <a href="#" class="time">
+                                        <i class="fa fa-clock-o"></i>
+                                        {{ event.time }}</a
+                                      >
+                                    </div>
+                                    <div class="col-12 col-md-2">
+                                      <a href="#" class="location">
+                                        <i class="fa fa-location-arrow"></i>
+                                        {{ event.location }}</a
+                                      >
+                                    </div>
+                                    <div class="d-flex col-12 col-md-5">
+                                      <a
+                                        href="event-details.html"
+                                        class="read-more ml-auto"
+                                        >READ MORE</a
+                                      >
+                                    </div>
+                                  </div>
+                                </div>
+                              </div>
+                              <!-- /.post-meta -->
                             </div>
                           </div>
-                          <!-- /.image-box -->
-                          <div class="post-meta">
-                            <h5 class="title">
-                              <a href="event-details.html"
-                                >International Law Enforcement Intellectual Property Crime Conference</a
-                              >
-                            </h5>
-                            <a href="event-details.html" class="read-more"
-                              >READ MORE</a
-                            >
-                          </div>
-                          <!-- /.post-meta -->
                         </div>
                         <!-- /.single-blog -->
+                        <hr class="hr" />
                       </div>
-                      <!-- /.col- -->
-                      <div class="col-md-4 col-12">
-                        <div class="single-blog">
-                          <div class="image-box">
-                            <img src="images/blog/3.jpg" alt="" />
-                            <div class="overlay">
-                              <a href="#" class="date">Feb 06, 2018</a>
-                            </div>
-                          </div>
-                          <!-- /.image-box -->
-                          <div class="post-meta">
-                            <h5 class="title">
-                              <a href="event-details.html"
-                                >International Law Enforcement Intellectual Property Crime Conference</a
-                              >
-                            </h5>
-                            <a href="event-details.html" class="read-more"
-                              >READ MORE</a
-                            >
-                          </div>
-                          <!-- /.post-meta -->
-                        </div>
-                        <!-- /.single-blog -->
-                      </div>
-                      <!-- /.col- -->
-                      <div class="col-md-4 col-12">
-                        <div class="single-blog">
-                          <div class="image-box">
-                            <img src="images/blog/3.jpg" alt="" />
-                            <div class="overlay">
-                              <a href="#" class="date">Feb 06, 2018</a>
-                            </div>
-                          </div>
-                          <!-- /.image-box -->
-                          <div class="post-meta">
-                            <h5 class="title">
-                              <a href="event-details.html"
-                                >International Law Enforcement Intellectual Property Crime Conference</a
-                              >
-                            </h5>
-                            <a href="event-details.html" class="read-more"
-                              >READ MORE</a
-                            >
-                          </div>
-                          <!-- /.post-meta -->
-                        </div>
-                        <!-- /.single-blog -->
-                      </div>
-                      <!-- /.col- -->
-                      <div class="col-md-4 col-12">
-                        <div class="single-blog">
-                          <div class="image-box">
-                            <img src="images/blog/3.jpg" alt="" />
-                            <div class="overlay">
-                              <a href="#" class="date">Feb 06, 2018</a>
-                            </div>
-                          </div>
-                          <!-- /.image-box -->
-                          <div class="post-meta">
-                            <h5 class="title">
-                              <a href="event-details.html"
-                                >International Law Enforcement Intellectual Property Crime Conference</a
-                              >
-                            </h5>
-                            <a href="event-details.html" class="read-more"
-                              >READ MORE</a
-                            >
-                          </div>
-                          <!-- /.post-meta -->
-                        </div>
-                        <!-- /.single-blog -->
-                      </div>
-                      <!-- /.col- -->
                     </div>
                   </div>
                   <!-- /.col- -->
@@ -134,143 +100,36 @@
                 <div class="row">
                   <div class="col-xl-12 col-lg-12 col-12 our-blog">
                     <div class="post-wrapper row px-4">
-                      <div class="col-md-4 col-12 mb-4">
+                      <div
+                        v-for="blog in blogs"
+                        :key="blog.title"
+                        class="col-md-4 col-12 mb-4"
+                      >
                         <div class="single-blog">
                           <div class="image-box">
                             <img src="images/blog/3.jpg" alt="" />
                             <div class="overlay">
-                              <a href="#" class="date">Feb 06, 2018</a>
+                              <a href="#" class="date">{{ blog.date }}</a>
                             </div>
                           </div>
                           <!-- /.image-box -->
                           <div class="post-meta">
                             <h5 class="title">
-                              <a href="news-details.html"
-                                >Trouble with the law since</a
+                              <a
+                                v-if="blog.title.length < 40"
+                                href="event-details.html"
+                                >{{ blog.title }}</a
                               >
+                              <a v-else href="event-details.html">{{
+                                blog.title.substring(0, 25) + "...."
+                              }}</a>
                             </h5>
                             <p>
                               To seek out new life and new civilizations to
                               boldly go where no man has gone before you would
                               see the biggest gift.
                             </p>
-                            <a href="news-details.html" class="read-more"
-                              >READ MORE</a
-                            >
-                          </div>
-                          <!-- /.post-meta -->
-                        </div>
-                        <!-- /.single-blog -->
-                      </div>
-                      <!-- /.col- -->
-                      <div class="col-md-4 col-12">
-                        <div class="single-blog">
-                          <div class="image-box">
-                            <img src="images/blog/3.jpg" alt="" />
-                            <div class="overlay">
-                              <a href="#" class="date">Feb 06, 2018</a>
-                            </div>
-                          </div>
-                          <!-- /.image-box -->
-                          <div class="post-meta">
-                            <h5 class="title">
-                              <a href="news-details.html"
-                                >Trouble with the law since</a
-                              >
-                            </h5>
-                            <p>
-                              To seek out new life and new civilizations to
-                              boldly go where no man has gone before you would
-                              see the biggest gift.
-                            </p>
-                            <a href="news-details.html" class="read-more"
-                              >READ MORE</a
-                            >
-                          </div>
-                          <!-- /.post-meta -->
-                        </div>
-                        <!-- /.single-blog -->
-                      </div>
-                      <!-- /.col- -->
-                      <div class="col-md-4 col-12">
-                        <div class="single-blog">
-                          <div class="image-box">
-                            <img src="images/blog/3.jpg" alt="" />
-                            <div class="overlay">
-                              <a href="#" class="date">Feb 06, 2018</a>
-                            </div>
-                          </div>
-                          <!-- /.image-box -->
-                          <div class="post-meta">
-                            <h5 class="title">
-                              <a href="news-details.html"
-                                >Trouble with the law since</a
-                              >
-                            </h5>
-                            <p>
-                              To seek out new life and new civilizations to
-                              boldly go where no man has gone before you would
-                              see the biggest gift.
-                            </p>
-                            <a href="news-details.html" class="read-more"
-                              >READ MORE</a
-                            >
-                          </div>
-                          <!-- /.post-meta -->
-                        </div>
-                        <!-- /.single-blog -->
-                      </div>
-                      <!-- /.col- -->
-                      <div class="col-md-4 col-12">
-                        <div class="single-blog">
-                          <div class="image-box">
-                            <img src="images/blog/3.jpg" alt="" />
-                            <div class="overlay">
-                              <a href="#" class="date">Feb 06, 2018</a>
-                            </div>
-                          </div>
-                          <!-- /.image-box -->
-                          <div class="post-meta">
-                            <h5 class="title">
-                              <a href="news-details.html"
-                                >Trouble with the law since</a
-                              >
-                            </h5>
-                            <p>
-                              To seek out new life and new civilizations to
-                              boldly go where no man has gone before you would
-                              see the biggest gift.
-                            </p>
-                            <a href="news-details.html" class="read-more"
-                              >READ MORE</a
-                            >
-                          </div>
-                          <!-- /.post-meta -->
-                        </div>
-                        <!-- /.single-blog -->
-                      </div>
-                      <!-- /.col- -->
-                      <div class="col-md-4 col-12">
-                        <div class="single-blog">
-                          <div class="image-box">
-                            <img src="images/blog/3.jpg" alt="" />
-                            <div class="overlay">
-                              <a href="#" class="date">Feb 06, 2018</a>
-                            </div>
-                          </div>
-                          <!-- /.image-box -->
-                          <div class="post-meta">
-                            <h5 class="title">
-                              <a href="news-details.html"
-                                >Trouble with the law since</a
-                              >
-                            </h5>
-                            <p>
-                              To seek out new life and new civilizations to
-                              boldly go where no man has gone before you would
-                              see the biggest gift.
-                            </p>
-                            <a href="news-details.html" class="read-more"
+                            <a href="event-details.html" class="read-more"
                               >READ MORE</a
                             >
                           </div>
@@ -308,3 +167,42 @@
     <!-- /.our-case -->
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      blogs: [
+        {
+          title: "International Law Enforcement Intellectual Property",
+          date: "Feb 06, 2023"
+        },
+        {
+          title: "International Law Enforcement",
+          date: "Feb 06, 2023"
+        },
+        {
+          title:
+            "International Law Enforcement Intellectual Property Crime Conference",
+          date: "Feb 06, 2023"
+        }
+      ],
+      events: [
+        {
+          title: "24th INTERPOL Asian Regional Conference",
+          date: "Tue 28 Feb, 2023",
+          time: "8:00",
+          location: "Phnom Penh"
+        },
+        {
+          title: "24th INTERPOL Asian Regional Conference",
+          date: "Tue 28 Feb, 2023",
+          time: "9:00",
+          location: "Phnom Penh"
+        }
+      ]
+    };
+  },
+  methods: {}
+};
+</script>
