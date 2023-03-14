96ab06593b28cd120344361b411f92349a499847
Mekmun Sopheaktra <Mekmunsopheaktra@gmail.com>
2023-03-14 16:59:19 +0700

    update

--- pages/service.vue
+++ pages/service.vue
@@ -1,7 +1,6 @@
 <template>
   <div>
     <TheInnerBanner title="Service" />
-
     <div class="our-solution section-spacing">
       <div class="container">
         <div class="theme-title-one">
@@ -25,7 +24,7 @@
             <!-- /.col- -->
             <div  @click="goToDetail" class="col-lg-4 col-sm-6 col-12">
               <div class="single-solution-block">
-                <img src="images/icon/8.png" alt="" class="icon" />
+                <img src="https://firebasestorage.googleapis.com/v0/b/apsara-fund.appspot.com/o/8.png?alt=media&token=8bba1e23-222e-4273-b917-b9827f749584" alt="" class="icon" />
                 <h5><a>Apsara Impact for Enterprises</a></h5>
                 <p>
                   Provide ESG consultation services that ensure our clients are competent, compliant, and certified (CCC) in terms of ESG practices.
@@ -36,7 +35,7 @@
             <!-- /.col- -->
             <div     @click="goToDetail" class="col-lg-4 col-sm-6 col-12">
               <div class="single-solution-block">
-                <img src="images/icon/6.png" alt="" class="icon" />
+                <img src="https://firebasestorage.googleapis.com/v0/b/apsara-fund.appspot.com/o/6.png?alt=media&token=c22d1f98-c9cc-42d5-9f63-84cef9ec4b6d" alt="" class="icon" />
                 <h5><a >Apara Investment  Matching Program</a></h5>
                 <p>
                   Facilitate investment pitching between small and growing ESG-oriented businesses and angel/impact investors.
@@ -47,7 +46,7 @@
             <!-- /.col- -->
             <div     @click="goToDetail" class="col-lg-6 col-sm-6 col-12">
               <div class="single-solution-block">
-                <img src="images/icon/9.png" alt="" class="icon" />
+                <img src="https://firebasestorage.googleapis.com/v0/b/apsara-fund.appspot.com/o/9.png?alt=media&token=75966de6-1dcf-43eb-bcca-064e347284c3" alt="" class="icon" />
                 <h5><a>Apsara Impact for NextGen</a></h5>
                 <p style="margin-left: 15%;margin-right: 15%;">
                   Run awareness workshops for university students, mainly in the business and digital technology field.
@@ -58,7 +57,7 @@
             <!-- /.col- -->
             <div     @click="goToDetail" class="col-lg-6 col-sm-6 col-12">
               <div class="single-solution-block">
-                <img src="images/icon/5.png" alt="" class="icon" />
+                <img src="https://firebasestorage.googleapis.com/v0/b/apsara-fund.appspot.com/o/5.png?alt=media&token=52819526-f493-4cf8-ad71-23d7098039bb" alt="" class="icon" />
                 <h5><a>Crowdfunding Platform</a></h5>
                 <p style="margin-left: 15%;margin-right: 15%;">
                   Create a trusted platform that is specifically designed for impact investments.
