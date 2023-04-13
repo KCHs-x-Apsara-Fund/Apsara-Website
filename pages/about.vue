b1817203f7c1dfa07fd225de9379590c3d32732c
Mekmun Sopheaktra <Mekmunsopheaktra@gmail.com>
2023-04-13 23:37:29 +0700

    about

--- pages/about.vue
+++ pages/about.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <TheInnerBanner title="About Us" />
+    <TheInnerBanner class="section-spacing-solution" title="About Us" />
 
     <!--
 			=============================================
